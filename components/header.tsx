c79a8153c409bc1804d7967afa8429d1fa47a386
meloinu <127707571+meloinu@users.noreply.github.com>
2023-09-13 14:29:59 -0600

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     return(
         <div className={styles.header}>
             <Image src={"/Thirdweb-Icon-Circle-Black-08 1.png"} alt="" height={48} width={48}/>
-            <p>Thirdweb PWA Template</p>
+            <p></p>
         </div>
     )
 };
